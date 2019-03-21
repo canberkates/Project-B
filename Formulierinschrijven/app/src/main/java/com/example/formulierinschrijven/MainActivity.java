f0b27f6aa713a48e95f594ad3aa827316e018918
Pranawa Somvedi <pranawab@gmail.com>
2019-03-21 11:49:07 +0100

    Branch Merge
    all branches merged into Pranawa

--- Formulierinschrijven/app/src/main/java/com/example/formulierinschrijven/MainActivity.java
+++ Formulierinschrijven/app/src/main/java/com/example/formulierinschrijven/MainActivity.java
@@ -8,16 +8,16 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-//    String name, lastname, email, date;
-//    int phone;
-//
-//    EditText inputName;
-//    EditText inputLastname;
-//    EditText inputPhone;
-//    EditText inputMail;
-//    EditText inputDate;
-//
-//    Button signUpButton;
+    String name, lastname, email, date;
+    int phone;
+
+    EditText inputName;
+    EditText inputLastname;
+    EditText inputPhone;
+    EditText inputMail;
+    EditText inputDate;
+
+    Button signUpButton;
 
 
     @Override
@@ -25,23 +25,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        inputName = (EditText) findViewById(R.id.inputName);
-//        inputLastname = (EditText) findViewById(R.id.inputLastname);
-//        inputPhone = (EditText) findViewById(R.id.inputPhone);
-//        inputMail = (EditText) findViewById(R.id.inputMail);
-//        inputDate = (EditText) findViewById(R.id.inputDate);
-//
-//        signUpButton = (Button) findViewById(R.id.signUpButton);
-//        signUpButton.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//               name = inputName.getText().toString();
-//               lastname = inputLastname.getText().toString();
-//               phone = Integer.valueOf(inputPhone.getText().toString());
-//               email = inputMail.getText().toString();
-//               date = inputDate.getText().toString();
-//            }
-//        });
+        inputName = (EditText) findViewById(R.id.inputName);
+        inputLastname = (EditText) findViewById(R.id.inputLastname);
+        inputPhone = (EditText) findViewById(R.id.inputPhone);
+        inputMail = (EditText) findViewById(R.id.inputMail);
+        inputDate = (EditText) findViewById(R.id.inputDate);
+
+        signUpButton = (Button) findViewById(R.id.signUpButton);
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               name = inputName.getText().toString();
+               lastname = inputLastname.getText().toString();
+               phone = Integer.valueOf(inputPhone.getText().toString());
+               email = inputMail.getText().toString();
+               date = inputDate.getText().toString();
+            }
+        });
     }
 }
 
