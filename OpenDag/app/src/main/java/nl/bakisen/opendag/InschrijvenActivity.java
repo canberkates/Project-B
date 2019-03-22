980387978e987bbff312fe753c7c3061557f0310
Kayra Cengiz <k.cngz38@hotmail.com>
2019-03-22 12:10:17 +0100

    Activity add
    Toevoegen activity Inschrijven.

---
+++ OpenDag/app/src/main/java/nl/bakisen/opendag/InschrijvenActivity.java
@@ -0,0 +1,13 @@
+package nl.bakisen.opendag;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InschrijvenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inschrijven);
+    }
+}
