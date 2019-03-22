c9a08ad2738127e04326b6ac823107f48302ed26
Pranawa Somvedi <pranawab@gmail.com>
2019-03-22 16:41:18 +0100

    Detail overzicht layout
    eerste layout versie

---
+++ OpenDag/app/src/main/java/nl/bakisen/opendag/OpendagDetail.java
@@ -0,0 +1,23 @@
+package nl.bakisen.opendag;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class OpendagDetail extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_opendag_detail);
+
+
+
+    }
+
+}
