0c41ff7ea5f7ea4e55f7aab45df850c201a2f6bf
Kayra Cengiz <k.cngz38@hotmail.com>
2019-03-29 11:33:12 +0100

    Spinner
    Spinner laat app crashen. Geen reden waardoor die crashed hij crashed gwn.

--- OpenDag/app/src/main/java/nl/bakisen/opendag/InschrijvenActivity.java
+++ OpenDag/app/src/main/java/nl/bakisen/opendag/InschrijvenActivity.java
@@ -8,9 +8,13 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+//import android.widget.Spinner;
 
 
 public class InschrijvenActivity extends AppCompatActivity{
+
+//    private Spinner spinner1, spinner2;
+//    private Button btnSubmit;
 
     String gender, name, lastname, email, date, education;
     int phone;
@@ -31,6 +35,10 @@ public class InschrijvenActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inschrijven);
 
+//        addListenerOnButton();
+//        addListenerOnSpinnerItemSelection();
+
+
         inputGender = (EditText) findViewById(R.id.inputGender);
         inputName = (EditText) findViewById(R.id.inputName);
         inputLastname = (EditText) findViewById(R.id.inputLastname);
@@ -39,30 +47,48 @@ public class InschrijvenActivity extends AppCompatActivity{
         inputDateBirth = (EditText) findViewById(R.id.inputDateBirth);
         inputEducation = (EditText) findViewById(R.id.inputEducation);
 
-        signUpButton = (Button) findViewById(R.id.signUpButton);
-        signUpButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                gender = inputGender.getText().toString();
-                name = inputName.getText().toString();
-                lastname = inputLastname.getText().toString();
-                phone = Integer.valueOf(inputPhone.getText().toString());
-                email = inputMail.getText().toString();
-                date = inputDateBirth.getText().toString();
-                education = inputEducation.getText().toString();
+//        signUpButton = (Button) findViewById(R.id.signUpButton);
 
-                showToast(gender);
-                showToast(name);
-                showToast(lastname);
-                showToast(String.valueOf(phone));
-                showToast(email);
-                showToast(date);
-                showToast(education);
+//    }
+//    public void addListenerOnSpinnerItemSelection() {
+//        spinner1 = (Spinner) findViewById(R.id.geslacht);
+//        spinner1.setOnItemSelectedListener(new CustomOnItemSelectedListener());
+//    }
+//
+//    public void addListenerOnButton() {
+//
+//        spinner1 = (Spinner) findViewById(R.id.geslacht);
+//        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+//
+//        btnSubmit.setOnClickListener(new View.OnClickListener() {
 
-            }
-        });
+        @Override
+        public void onClick(View v) {
 
+            gender = inputGender.getText().toString();
+            name = inputName.getText().toString();
+            lastname = inputLastname.getText().toString();
+            phone = Integer.valueOf(inputPhone.getText().toString());
+            email = inputMail.getText().toString();
+            date = inputDateBirth.getText().toString();
+            education = inputEducation.getText().toString();
+
+            showToast(gender);
+            showToast(name);
+            showToast(lastname);
+            showToast(String.valueOf(phone));
+            showToast(email);
+            showToast(date);
+            showToast(education);
+
+            Toast.makeText(InschrijvenActivity.this,
+                    "OnClickListener : " +
+                            "\nSpinner 1 : " + String.valueOf(spinner1.getSelectedItem()),
+                    Toast.LENGTH_SHORT).show();
+        }
+    });
     }
+
     public void showToast(String text) {
         Toast.makeText(InschrijvenActivity.this, text, Toast.LENGTH_SHORT).show();
     }
