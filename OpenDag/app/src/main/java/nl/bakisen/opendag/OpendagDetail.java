eed545f2a444d946b290dfc265a8c5962f1f3fed
Pranawa Somvedi <pranawab@gmail.com>
2019-03-30 19:30:53 +0100

    reset
    .

--- OpenDag/app/src/main/java/nl/bakisen/opendag/OpendagDetail.java
+++ OpenDag/app/src/main/java/nl/bakisen/opendag/OpendagDetail.java
@@ -3,13 +3,12 @@ package nl.bakisen.opendag;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class MainActivity extends Fragment {
+public class OpendagDetail extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
