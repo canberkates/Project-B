ff8aeeaeb03008b741d5389678f418e86c5029de
enakinci <45963021+enakinci@users.noreply.github.com>
2019-04-28 16:08:40 +0200

    lemao

--- OpenDag/app/src/main/java/nl/bakisen/opendag/OverzichtFragment.java
+++ OpenDag/app/src/main/java/nl/bakisen/opendag/OverzichtFragment.java
@@ -1,33 +1,34 @@
 package nl.bakisen.opendag;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
-public class OverzichtFragment extends AppCompatActivity {
-    private Button opendag1overzicht;
-
-
-
+public class OverzichtFragment extends Fragment {
+    @Nullable
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_overzicht);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        opendag1overzicht = (Button) findViewById(R.id.button);
-        opendag1overzicht.setOnClickListener(new View.OnClickListener() {
+        View view = inflater.inflate(R.layout.fragment_overzicht, container, false);
+
+        Button opendag1 = (Button) view.findViewById(R.id.opendag1);
+        opendag1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                openOverzicht2();
-
+                FragmentTransaction fropendag1 = getFragmentManager().beginTransaction();
+                fropendag1.replace(R.id.fragment_container, new OverzichtOpenDag1Fragment());
+                fropendag1.commit();
             }
         });
-    }
 
-    public void openOverzicht2() {
-        Intent intent = new Intent(this, OverzichtOpenDag1.class);
-        startActivity(intent);
+        return view;
+
     }
 }
