a8d245a8ccf5fe88ff76afaee077b9655b0f2787
canberkates <45592566+canberkates@users.noreply.github.com>
2019-05-23 19:52:28 +0200

    popupdate

--- OpenDag/app/src/main/java/nl/bakisen/opendag/Pop.java
+++ OpenDag/app/src/main/java/nl/bakisen/opendag/Pop.java
@@ -1,16 +1,22 @@
 package nl.bakisen.opendag;
 
 import android.app.Activity;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.ScaleGestureDetector;
 import android.view.WindowManager;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.view.View;
 
 public class Pop extends Activity {
 
     int background;
+    Matrix matrix = new Matrix();
+    Float scale = 1f;
+    ScaleGestureDetector SGD;
 
     protected void onCreate(Bundle savedInstanceState){
 
@@ -19,48 +25,69 @@ public class Pop extends Activity {
         setContentView(R.layout.popwindow);
         Bundle extras = getIntent().getExtras();
         RelativeLayout layout = findViewById(R.id.popwindow);
-        View view = getLayoutInflater().inflate(R.layout.popwindow, null);
-        setContentView(view);
-        view.setOnClickListener(new View.OnClickListener() {
-            float zoomFactor = 1.3f;
-            boolean zoomedOut = false;
+        final ImageView zoombackground = (ImageView) findViewById(R.id.zoombackground);
+        /*View view = getLayoutInflater().inflate(R.layout.popwindow, null);*/
+        /*view.setOnClickListener(new View.OnClickListener() {*/
+        /*    float zoomFactor = 1.3f;*/
+        /*    boolean zoomedOut = false;*/
+/*
+*/
 
-            @Override
-            public void onClick(View v) {
-                if(zoomedOut) {
-                    v.setScaleX(1);
-                    v.setScaleY(1);
-                    zoomedOut = false;
-                }
-                else {
-                    v.setScaleX(zoomFactor);
-                    v.setScaleY(zoomFactor);
-                    zoomedOut = true;
-                }
-            }
-        });
+        /*    @Override*/
+        /*    public void onClick(View v) {*/
+        /*        if(zoomedOut) {*/
+        /*            v.setScaleX(1);*/
+        /*            v.setScaleY(1);*/
+        /*            zoomedOut = false;*/
+        /*        }*/
+        /*        else {*/
+        /*            v.setScaleX(zoomFactor);*/
+        /*            v.setScaleY(zoomFactor);*/
+        /*            zoomedOut = true;*/
+        /*        }*/
+        /*    }*/
+        /*});*/
         if (extras != null) {
             background = extras.getInt("plattegrond");
         }
 
         if (background == 1) {
-            layout.setBackgroundResource(R.drawable.beganegrond);
+            zoombackground.setImageResource(R.drawable.beganegrond);
+            /*layout.setBackgroundResource(R.drawable.beganegrond);*/
         }
         else if (background == 2) {
-            layout.setBackgroundResource(R.drawable.etage1);
+            zoombackground.setImageResource(R.drawable.etage1);
+            /*layout.setBackgroundResource(R.drawable.etage1);*/
         }
         else if (background == 3) {
-            layout.setBackgroundResource(R.drawable.etage2);
+            zoombackground.setImageResource(R.drawable.etage2);
+            /*layout.setBackgroundResource(R.drawable.etage2);*/
         }
         else if (background == 4) {
-            layout.setBackgroundResource(R.drawable.etage3);
+            zoombackground.setImageResource(R.drawable.etage3);
+            /*layout.setBackgroundResource(R.drawable.etage3);*/
         }
         else if (background == 5) {
-            layout.setBackgroundResource(R.drawable.etage4);
+            zoombackground.setImageResource(R.drawable.etage4);
+            /*layout.setBackgroundResource(R.drawable.etage4);*/
         }
         else if (background == 6) {
-            layout.setBackgroundResource(R.drawable.etage5);
+            zoombackground.setImageResource(R.drawable.etage5);
+            /*layout.setBackgroundResource(R.drawable.etage5);*/
         }
+
+        private class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener{
+            @Override
+            public boolean onScale(ScaleGestureDetector detector){
+                scale = scale * detector.getScaleFactor();
+                scale = Math.max(0.1f, Math.min(scale, 5f));
+                matrix.setScale(scale, scale);
+                zoombackground.setImageMatrix(matrix);
+                return true;
+                
+            }
+        }
+        
 
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
@@ -68,7 +95,7 @@ public class Pop extends Activity {
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int)(width),(int)(height*.6));
+        getWindow().setLayout((int)(width),(int)(height*.2));
 
         WindowManager.LayoutParams params = getWindow().getAttributes();
         params.gravity = Gravity.CENTER;
