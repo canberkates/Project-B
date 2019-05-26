ed8d56f37644bc7aa87da4c6741ef079064accb5
canberkates <45592566+canberkates@users.noreply.github.com>
2019-05-26 22:14:00 +0200

    correcte zoom

--- OpenDag/app/src/main/java/nl/bakisen/opendag/Pop.java
+++ OpenDag/app/src/main/java/nl/bakisen/opendag/Pop.java
@@ -1,14 +1,17 @@
 package nl.bakisen.opendag;
 
 import android.app.Activity;
+import android.graphics.Matrix;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.ScaleGestureDetector;
 import android.view.WindowManager;
+import android.widget.HorizontalScrollView;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 
 public class Pop extends Activity {
 
@@ -18,47 +21,49 @@ public class Pop extends Activity {
     private ScaleGestureDetector SGD;
 
     protected void onCreate(Bundle savedInstanceState){
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.popwindow);
         Bundle extras = getIntent().getExtras();
         RelativeLayout layout = findViewById(R.id.popwindow);
         zoombackground = (ImageView) findViewById(R.id.zoombackground);
-        
+
 
         SGD = new ScaleGestureDetector(this, new ScaleListener());
-
         if (extras != null) {
             background = extras.getInt("plattegrond");
         }
 
         if (background == 1) {
-            layout.setBackgroundResource(R.drawable.beganegrond);
+            zoombackground.setImageResource(R.drawable.beganegrond);
+            /*layout.setBackgroundResource(R.drawable.beganegrond);*/
         }
         else if (background == 2) {
-            layout.setBackgroundResource(R.drawable.etage1);
+            zoombackground.setImageResource(R.drawable.etage1);
+            /*layout.setBackgroundResource(R.drawable.etage1);*/
         }
         else if (background == 3) {
-            layout.setBackgroundResource(R.drawable.etage2);
+            zoombackground.setImageResource(R.drawable.etage2);
+            /*layout.setBackgroundResource(R.drawable.etage2);*/
         }
         else if (background == 4) {
-            layout.setBackgroundResource(R.drawable.etage3);
+            zoombackground.setImageResource(R.drawable.etage3);
+            /*layout.setBackgroundResource(R.drawable.etage3);*/
         }
         else if (background == 5) {
-            layout.setBackgroundResource(R.drawable.etage4);
+            zoombackground.setImageResource(R.drawable.etage4);
+            /*layout.setBackgroundResource(R.drawable.etage4);*/
         }
         else if (background == 6) {
-            layout.setBackgroundResource(R.drawable.etage5);
+            zoombackground.setImageResource(R.drawable.etage5);
+            /*layout.setBackgroundResource(R.drawable.etage5);*/
         }
-
         DisplayMetrics dm = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(dm);
 
         int width = dm.widthPixels;
         int height = dm.heightPixels;
 
-        getWindow().setLayout((int)(width),(int)(height*.6));
+        getWindow().setLayout((int)(width),(int)(height*.8));
 
         WindowManager.LayoutParams params = getWindow().getAttributes();
         params.gravity = Gravity.CENTER;
